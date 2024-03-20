1bf5aa352d8a2a90e86a1e13029fd51616e1d195
nokm4n <44049305+nokm4n@users.noreply.github.com>
2024-03-20 21:50:00 +0400

    Basic hit effect

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField, NotNull] private Rigidbody2D _rb;
+	[SerializeField, NotNull] GameObject _hitEffect;
     [SerializeField] private float force;
 
 	private void Awake()
 	{
 		_rb.AddForce(transform.up * force, ForceMode2D.Impulse);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		GameObject hitEffect = Instantiate(_hitEffect, transform.position, Quaternion.identity);
+		Destroy(hitEffect, 5f);
+		Destroy(gameObject);
 	}
 }
