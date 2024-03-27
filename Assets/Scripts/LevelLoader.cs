fc308baf76935f163c8ea0a5498826cbb2b90565
nokm4n <44049305+nokm4n@users.noreply.github.com>
2024-03-27 21:31:52 +0400

    level utilities

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _levels;
+
+	private void Awake()
+	{
+		//Get save, cur lvl and load it
+	}
+
+	//Spawn new level
+}
