66fa12552489decb8abe4885e988633e248032b6
nokm4n <44049305+nokm4n@users.noreply.github.com>
2024-04-30 21:50:59 +0400

    TestAbility

---
+++ Assets/Scripts/Ability/AbilityBase.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbilityBase : MonoBehaviour
+{
+	protected AttackType AttackType;
+	protected AbilityType AbilityType;
+	protected AttackRangeType AttackRangeType;
+	protected DamageType DamageType;
+}
+public enum AttackType
+{
+	OneAttack,
+	Autoattack
+}
+
+public enum AbilityType
+{
+	DamageAbility,
+	HealAbility
+}
+
+public enum AttackRangeType
+{
+	MeleeAttack,
+	RangeAttack,
+	Inner
+}
+
+public enum DamageType
+{
+	Magical,
+	Physical
+}
